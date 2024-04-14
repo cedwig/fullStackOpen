46037cbd00b3340664418bca1436cc5b14b36577
Ludwig.eth <ludwig.eth@gmail.com>
2024-04-14 22:13:02 +0200

    .

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -22,7 +22,7 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
