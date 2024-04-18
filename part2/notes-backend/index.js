52dc6b7add58634f012d6ea258a06967a7d52dcb
Ludwig.eth <ludwig.eth@gmail.com>
2024-04-18 23:40:42 +0200

    Solution exercise 3.22

--- part2/notes-backend/index.js
+++ part2/notes-backend/index.js
@@ -1,107 +1,117 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
-const password = encodeURI(process.argv[2])
+morgan.token('postData', (req) => {
+  return JSON.stringify(req.body)
+})
 
-const Note = require('./models/note')
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
+app.use(requestLogger)
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+
 
 let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
 ]
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
-  
+
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-  
-app.post('/api/notes', (request, response) => {
-    const body = request.body
 
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-    console.log('backend');
-    notes = notes.concat(note)
-  
-    response.json(note)
-})
+app.use(unknownEndpoint)
 
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body;
-  const id = Number(request.params.id);
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-  const noteIndex = notes.findIndex((note) => note.id === id);
-  
-  if (noteIndex !== -1) {
-    notes[noteIndex].content = body.content || notes[noteIndex].content;
-    notes[noteIndex].important = !notes[noteIndex].important;
-
-    response.json(notes[noteIndex]);
-  } else {
-    response.status(404).json({ error: 'Note not found' });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-});
 
-  
-const PORT = process.env.PORT || 3001
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part2/notes-backend/mongo.js
+++ part2/notes-backend/mongo.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
-const url = `mongodb+srv://ludwigeth:${password}@ludwig.kys7mwt.mongodb.net/noteApp?retryWrites=true&w=majority`
+// const url = `mongodb+srv://ludwigeth:${password}@ludwig.kys7mwt.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+// mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'third',
-  date: new Date(),
-  important: true,
-})
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: String,
+//   important: Boolean,
 // })
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+// const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'third',
+//   date: new Date(),
+//   important: true,
+// })
+
+// // note.save().then(result => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
