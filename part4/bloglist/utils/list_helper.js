b160c8bee504efb2bea0d02f72adc90c54f2efbc
Ludwig.eth <ludwig.eth@gmail.com>
2024-04-25 21:28:20 +0200

    Solution exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,25 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined
+  return blogs.reduce((acc, blog) => {
+    return (acc.likes > blog.likes) ? acc : blog
+  })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
